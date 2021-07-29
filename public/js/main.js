ab26b5cebd6faae41788b7fff94bd7a97059649a
Rafadmartins <contato.rafaelmartinsblv@gmail.com>
2021-07-29 13:29:49 -0300

    add header, banner and js

---
+++ public/js/main.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper('.swiper-container', {
+    slidesPerView: 1,
+    pagination: {
+      el: '.swiper-pagination'
+    },
+    mousewheel: true,
+    keyboard: true
+      });
+
+ 
