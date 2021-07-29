2435659bd98b3a14c92ddb8c378e92c9dcf5d95a
LeoScripts <leandroguitarjesus89@gmail.com>
2021-07-29 00:51:12 -0300

    start of styling the categories page

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.static('public'));
+
 app.get('/home', (req,res)=>{
     res.sendFile(__dirname + '/views/pages/home.html')
 })
