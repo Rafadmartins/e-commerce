6ee6eb86d80604d8b858647da23e4b9ef81fe784
Douglas Costa | TechPartners BR <dcosta@techpartnersbr.com>
2021-07-30 23:10:29 -0300

    Add section destaques do dia

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,18 @@
 const swiper = new Swiper('.swiper-container', {
-  slidesPerView: 1,
+  // Optional parameters
+  // direction: 'vertical',
+  loop: true,
+
+  // If we need pagination
   pagination: {
     el: '.swiper-pagination'
   },
-  mousewheel: true,
-  keyboard: true
-    });
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev'
+  }
+
+  // And if we need scrollbar
+})
