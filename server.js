a39049cc9999d28c111ac28f77854e82d7752bf8
Gabriel Renovato <oficialalfeim@outlook.com>
2021-08-05 21:35:14 -0300

    Installation of the ejs library, changes to all html files, server changes and changes to categories.css

--- server.js
+++ server.js
@@ -1,16 +1,23 @@
 const express = require('express');
+const path = require('path')
 
 const app = express();
 
 app.use(express.static('public'));
+app.set('view engine', 'ejs');
+app.set('views', path.resolve("views"));
+
 
 app.get('/home', (req,res)=>{
-    res.sendFile(__dirname + '/views/pages/home.html')
+    res.render(path.resolve("views/pages", "home"))
 })
 
 app.get('/categorias', (req,res)=>{
-    res.sendFile(__dirname + '/views/pages/categorias.html')
+    res.render(path.resolve("views/pages", "categorias"))
 })
 
+app.get('/detalhes', (req,res)=>{
+    res.render(path.resolve("views/pages", "detalhes"))
+})
 
 app.listen(3000);
